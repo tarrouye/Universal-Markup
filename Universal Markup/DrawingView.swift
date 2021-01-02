50b0ab7f7d60612c3279c135a3cf69b96c0dc3e5
Théo Arrouye <theo@arrouye.net>
2021-01-01 22:05:27 -0800

    Make capture button work. Contents copied to pasteboard.

--- Universal Markup/DrawingView.swift
+++ Universal Markup/DrawingView.swift
@@ -9,6 +9,16 @@ import Foundation
 import Cocoa
 import SwiftUI
 
+extension CGImage {
+    var png: Data? {
+        guard let mutableData = CFDataCreateMutable(nil, 0),
+            let destination = CGImageDestinationCreateWithData(mutableData, "public.png" as CFString, 1, nil) else { return nil }
+        CGImageDestinationAddImage(destination, self, nil)
+        guard CGImageDestinationFinalize(destination) else { return nil }
+        return mutableData as Data
+    }
+}
+
 struct BrushStroke {
     var width : CGFloat
     var color : CGColor
@@ -25,12 +35,16 @@ class DrawingView: NSView {
     //var borderColor : CGColor = CGColor(red: 0.816, green: 0.843, blue: 0.902, alpha: 1.000)
     var borderColor = CGColor.black
     
+    var capturedBorderColor = CGColor(red: 0, green: 1, blue: 0, alpha: 1)
+    
     var autoStraightenLines : Bool = ToolsDefaults.detectLines
     var straightLineThreshold : CGFloat = ToolsDefaults.straightLineThreshold
     
     
     var brushStrokes = [BrushStroke]()
     var currentStroke : Int?
+    
+    var capturedWindowImage : CGImage?
     
     // initializers
     // (initialize super + register as observer to notifications)
@@ -50,10 +64,103 @@ class DrawingView: NSView {
         NotificationCenter.default.addObserver(self, selector: #selector(self.setStraightenLines(notif:)), name: Notification.Name("SetStraightenLines"), object: nil)
         
         NotificationCenter.default.addObserver(self, selector: #selector(self.selectedDefaultPen(notif:)), name: Notification.Name("SelectedDefaultPen"), object: nil)
+        
+        NotificationCenter.default.addObserver(self, selector: #selector(self.captureButtonClicked(notif:)), name: Notification.Name("CaptureButtonClicked"), object: nil)
+        
+        NotificationCenter.default.addObserver(self, selector: #selector(self.clearCapture(notif:)), name: Notification.Name("ClearButtonClicked"), object: nil)
     }
+    
+    
     
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
+    }
+    
+    // enables ability to resize window, as well as window floating on top
+    func enableCaptureBehavior() {
+        // enable resizing
+        self.window!.styleMask.insert(.resizable)
+        
+        // enable floating
+        self.window!.level = .floating
+    }
+    
+    // removes ability to resize window, as well as window floating on top
+    func disableCaptureBehavior() {
+        // remove ability to resize window
+        self.window!.styleMask.remove(.resizable)
+        
+        // stop floating
+        self.window!.level = .normal
+    }
+    
+    // calback for ClearButtonClicked notification
+    @objc func clearCapture(notif: NSNotification) {
+        // clear capture
+        capturedWindowImage = nil
+        
+        // clear brushstrokes
+        brushStrokes.removeAll()
+        
+        // enable capturing behavior (floating, resizing)
+        enableCaptureBehavior()
+        
+        // set needs display
+        self.needsDisplay = true
+    }
+    
+    // callback for CaptureButtonClicked notification
+    // captures the drawing frame
+    @objc func captureButtonClicked(notif: NSNotification) {
+        // get capture window bounds in screen coordinates
+        let screenCoords = (self.window?.convertToScreen(self.frame))! as CGRect
+        // get screen height
+        var screenHeight : CGFloat = 0
+        if let screen = NSScreen.main {
+            let rect = screen.frame
+            screenHeight = rect.size.height
+        }
+        // guard bad value
+        if (screenHeight == 0) {
+            return
+        }
+        
+        // invert y coordinate (thats how CGWindowListCreateImage wants it..)
+        let flippedScreenCoords = CGRect(x: screenCoords.minX + borderWidth, y: screenHeight - screenCoords.minY - screenCoords.height + borderWidth, width: screenCoords.width - borderWidth * 2, height: screenCoords.height - borderWidth * 2)
+        
+        // get capture window ID
+        let windowID = CGWindowID(self.window!.windowNumber)
+        
+        // capture our image
+        let windowImage: CGImage? = CGWindowListCreateImage(flippedScreenCoords, [.optionIncludingWindow, .optionOnScreenBelowWindow], windowID, [.bestResolution])
+        
+        // store it
+        capturedWindowImage = windowImage
+        
+        // display it
+        self.needsDisplay = true
+        
+        // switch off capturing behaviors
+        disableCaptureBehavior()
+        
+        // copy the image to pasteboard
+        let pasteboard = NSPasteboard.general
+        let pasteboardItem = NSPasteboardItem()
+        pasteboardItem.setData((windowImage?.png)!, forType: .png)
+        
+        pasteboard.clearContents()
+        pasteboard.writeObjects([pasteboardItem])
+        
+        // alert user about pasteboard
+        let al = NSAlert()
+        al.informativeText = "Your capture has been copied to the pasteboard!"
+        al.messageText = "Paste away!"
+        al.showsHelp = false
+        // use a delay because otherwise swiftUI wont refresh (change capture button to clear) until alert is dismissed
+        Timer.scheduledTimer(withTimeInterval: 0.01, repeats: false) { (nil) in
+            al.runModal()
+        }
+        
     }
     
     // callback for SetWindowOpacity notifcation
@@ -254,40 +361,46 @@ class DrawingView: NSView {
            return
         }
         
-        // background w/ variable opacity (fill) + window frame (stroke)
-        context.setFillColor(CGColor(red: 1, green: 1, blue: 1, alpha: CGFloat(windowOpacity)))
-        context.fill(bounds)
-        context.setStrokeColor(borderColor)
+        // window frame (stroke)
+        context.setStrokeColor(self.capturedWindowImage == nil ? borderColor : capturedBorderColor)
         context.setLineWidth(borderWidth)
         context.setFillColor(CGColor.clear)
         context.stroke(bounds.insetBy(dx: borderWidth / 2, dy: borderWidth / 2))
         
-        
-        // draw brush strokes
-        for stroke in brushStrokes {
-            // set the stroke width
-            context.setLineWidth(stroke.width)
-            context.setLineCap(.round)
+        if self.capturedWindowImage != nil {
+            context.draw(self.capturedWindowImage!, in: dirtyRect.insetBy(dx: borderWidth, dy: borderWidth))
+        } else {
+            // background w/ variable opacity (fill)
+            context.setFillColor(CGColor(red: 1, green: 1, blue: 1, alpha: CGFloat(windowOpacity)))
+            context.fill(bounds)
             
-            // begin path
-            context.beginPath()
             
-            // go through stroke points to create path
-            if (stroke.points.count > 2) {
-                context.move(to: stroke.points[0])
-                for i in 1...stroke.points.count - 1 {
-                    context.addLine(to: stroke.points[i])
+            // draw brush strokes
+            for stroke in brushStrokes {
+                // set the stroke width
+                context.setLineWidth(stroke.width)
+                context.setLineCap(.round)
+                
+                // begin path
+                context.beginPath()
+                
+                // go through stroke points to create path
+                if (stroke.points.count > 2) {
+                    context.move(to: stroke.points[0])
+                    for i in 1...stroke.points.count - 1 {
+                        context.addLine(to: stroke.points[i])
+                    }
+                } else if (stroke.points.count == 2) {
+                    context.move(to: stroke.points[0])
+                    context.addLine(to: stroke.points[1])
+                } else {
+                    continue
                 }
-            } else if (stroke.points.count == 2) {
-                context.move(to: stroke.points[0])
-                context.addLine(to: stroke.points[1])
-            } else {
-                continue
+                
+                // set color and draw the stroke
+                context.setStrokeColor(stroke.color)
+                context.strokePath()
             }
-            
-            // set color and draw the stroke
-            context.setStrokeColor(stroke.color)
-            context.strokePath()
         }
         
     }
--- Universal Markup/ToolView.swift
+++ Universal Markup/ToolView.swift
@@ -18,6 +18,8 @@ struct ToolView: View {
     
     @State var hoveringCaptureButton: Bool = false
     
+    @State var captureNotClear: Bool = true
+    
     func bgOpacityChanged() {
         NotificationCenter.default.post(name: Notification.Name("SetWindowOpacity"), object: nil, userInfo: ["value" : bgOpacity])
     }
@@ -50,141 +52,176 @@ struct ToolView: View {
         penSize = ToolsDefaults.defaultPens[pen].strokeWidth
     }
     
+    func captureButtonClicked() {
+        if self.captureNotClear {
+            NotificationCenter.default.post(name: Notification.Name("CaptureButtonClicked"), object: nil, userInfo: nil)
+            self.winMovable = true
+            winPickerChanged()
+        } else  {
+            NotificationCenter.default.post(name: Notification.Name("ClearButtonClicked"), object: nil, userInfo: nil)
+        }
+        
+        self.captureNotClear = !self.captureNotClear
+    }
+    
     var body: some View {
         VStack(alignment: .leading) {
-            // [GROUP] CAPTURE FRAME CONTROLS
-            Group {
-                Text("Capture Frame Controls")
-                    .font(.headline)
-                    .fontWeight(.bold)
-                    .padding(.bottom)
-                
-                // Background Opacity Slider
-                VStack(alignment: .leading) {
-                    Text("Background Opacity")
-                    
-                    Slider(value: Binding(get: {
-                        self.bgOpacity
-                    }, set: { (newVal) in
-                        self.bgOpacity = newVal
-                        self.bgOpacityChanged()
-                    }), in: 0...1, step: 0.1)
-                }
-                .frame(maxWidth: 300)
-                .padding(.bottom)
-                
-                // Frame Width Slider
-                VStack(alignment: .leading) {
-                    Text("Frame Width")
-                    
-                    Slider(value: Binding(get: {
-                        self.frameThickness
-                    }, set: { (newVal) in
-                        self.frameThickness = newVal
-                        self.frameThicknessChanged()
-                    }), in: 1...20, step: 0.1)
-                }
-                .frame(maxWidth: 300)
-                .padding(.bottom)
-                
-                // Window Movability Toggle
-                Picker(selection: Binding(get: {
-                    self.winMovable
-                }, set: { (newVal) in
-                    self.winMovable = newVal
-                    self.winPickerChanged()
-                }), label: Text("Frame Dragging:")) {
-                    Text("Enabled").tag(true)
-                    Text("Disabled").tag(false)
-                }.pickerStyle(RadioGroupPickerStyle())
-                .padding(.bottom)
-            }
-            
-            Divider()
-            
-            // [GROUP] PEN CONTROLS
-            Group {
-                Text("Pen Controls")
-                    .font(.headline)
-                    .fontWeight(.bold)
-                    .padding(.bottom)
-                
-                // Stroke Width Slider
-                VStack(alignment: .leading) {
-                    Text("Stroke Width")
-                    
-                    Slider(value: $penSize, in: 1...50, step: 0.1, onEditingChanged: { editing in
-                        if (!editing) {
-                            self.penSizeChanged()
+            if (self.captureNotClear) {
+                // [GROUP] ALL CAPTURE CONTROLS
+                Group {
+                    // [GROUP] CAPTURE FRAME CONTROLS
+                    Group {
+                        Text("Capture Frame Controls")
+                            .font(.headline)
+                            .fontWeight(.bold)
+                            .padding(.bottom)
+                        
+                        // Background Opacity Slider
+                        VStack(alignment: .leading) {
+                            Text("Background Opacity")
+                            
+                            Slider(value: Binding(get: {
+                                self.bgOpacity
+                            }, set: { (newVal) in
+                                self.bgOpacity = newVal
+                                self.bgOpacityChanged()
+                            }), in: 0...1, step: 0.1)
                         }
-                    })
-                }
-                .frame(maxWidth: 300)
-                .padding(.bottom)
-                
-                // Stroke color picker
-                ColorPicker("Stroke Color", selection: Binding(get: {
-                    self.penColor
-                }, set: { (newVal) in
-                    self.penColor = newVal
-                    self.penColorChanged()
-                }), supportsOpacity: true)
-                
-                // Line Detection Toggle
-                Picker(selection: Binding(get: {
-                    self.detectLines
-                }, set: { (newVal) in
-                    self.detectLines = newVal
-                    self.detectLinesChanged()
-                }), label: Text("Auto-Straighten Lines:")) {
-                    Text("Enabled").tag(true)
-                    Text("Disabled").tag(false)
-                }.pickerStyle(RadioGroupPickerStyle())
-                .padding(.bottom)
-                
-                
-                // Default pen options
-                VStack(alignment: .leading) {
-                    Text("Templates")
-                        .font(.headline)
-                    
-                    HStack {
-                        // Pen button
-                        Button(action: {
-                            selectedDefaultPen(0)
-                        }) {
-                            Text("Pen")
+                        .frame(maxWidth: 300)
+                        .padding(.bottom)
+                        
+                        // Frame Width Slider
+                        VStack(alignment: .leading) {
+                            Text("Frame Width")
+                            
+                            Slider(value: Binding(get: {
+                                self.frameThickness
+                            }, set: { (newVal) in
+                                self.frameThickness = newVal
+                                self.frameThicknessChanged()
+                            }), in: 1...20, step: 0.1)
+                        }
+                        .frame(maxWidth: 300)
+                        .padding(.bottom)
+                        
+                        // Window Movability Toggle
+                        HStack {
+                            Picker(selection: Binding(get: {
+                                self.winMovable
+                            }, set: { (newVal) in
+                                self.winMovable = newVal
+                                self.winPickerChanged()
+                            }), label: Text("Frame Dragging:")) {
+                                Text("Enabled").tag(true)
+                                Text("Disabled").tag(false)
+                            }.pickerStyle(RadioGroupPickerStyle())
+                            .padding(.bottom)
+                            
+                            
+                            Divider()
+                            Spacer()
+                            
+                            Text("Inking \(winMovable ? "Disabled" : "Enabled")")
+                                .fontWeight(.bold)
                         }
                         
-                        // Highlighter button
-                        Button(action: {
-                            selectedDefaultPen(1)
-                        }) {
-                            Text("Highlighter")
+                        Text("Note: Inking is Enabled/Disabled based on the selected value for Frame Dragging.")
+                            .font(.caption)
+                            .lineLimit(2)
+                            .multilineTextAlignment(.leading)
+                            .padding(.horizontal)
+                            .fixedSize(horizontal: false, vertical: true)
+                    }
+                    
+                    Divider()
+                    
+                    // [GROUP] PEN CONTROLS
+                    Group {
+                        Text("Pen Controls")
+                            .font(.headline)
+                            .fontWeight(.bold)
+                            .padding(.bottom)
+                        
+                        // Stroke Width Slider
+                        VStack(alignment: .leading) {
+                            Text("Stroke Width")
+                            
+                            Slider(value: $penSize, in: 1...50, step: 0.1, onEditingChanged: { editing in
+                                if (!editing) {
+                                    self.penSizeChanged()
+                                }
+                            })
+                        }
+                        .frame(maxWidth: 300)
+                        .padding(.bottom)
+                        
+                        // Stroke color picker
+                        ColorPicker("Stroke Color", selection: Binding(get: {
+                            self.penColor
+                        }, set: { (newVal) in
+                            self.penColor = newVal
+                            self.penColorChanged()
+                        }), supportsOpacity: true)
+                        
+                        // Line Detection Toggle
+                        Picker(selection: Binding(get: {
+                            self.detectLines
+                        }, set: { (newVal) in
+                            self.detectLines = newVal
+                            self.detectLinesChanged()
+                        }), label: Text("Auto-Straighten Lines:")) {
+                            Text("Enabled").tag(true)
+                            Text("Disabled").tag(false)
+                        }.pickerStyle(RadioGroupPickerStyle())
+                        .padding(.bottom)
+                        
+                        
+                        // Default pen options
+                        VStack(alignment: .leading) {
+                            Text("Templates")
+                                .font(.headline)
+                            
+                            HStack {
+                                // Pen button
+                                Button(action: {
+                                    selectedDefaultPen(0)
+                                }) {
+                                    Text("Pen")
+                                }
+                                
+                                // Highlighter button
+                                Button(action: {
+                                    selectedDefaultPen(1)
+                                }) {
+                                    Text("Highlighter")
+                                }
+                            }
                         }
                     }
+                    
+                    Divider()
                 }
+            } else {
+                EmptyView()
             }
-            
-            Divider()
             
             HStack {
                 Spacer()
             
                 // Capture button
-                Text("Capture")
+                Text(captureNotClear ? "Capture" : "Clear")
                     .padding()
-                    .background(hoveringCaptureButton ? Color(red: 0.0, green: 0.5, blue: 1.0, opacity: 0.75) : Color.blue)
+                    .background(hoveringCaptureButton ? Color(red: 0.0, green: 0.5, blue: 1.0, opacity: 0.75) : (captureNotClear ? Color.blue : Color.red))
                     .foregroundColor(Color.white)
                     .font(.title2)
                     .cornerRadius(100)
                     .padding(5)
                     .overlay(
                         RoundedRectangle(cornerRadius: 100)
-                            .stroke(hoveringCaptureButton ? Color(red: 0.0, green: 0.5, blue: 1.0, opacity: 0.75) : Color.blue, lineWidth: 5)
+                            .stroke(hoveringCaptureButton ? Color(red: 0.0, green: 0.5, blue: 1.0, opacity: 0.75) : (captureNotClear ? Color.blue : Color.red), lineWidth: 5)
                     )
                     .onTapGesture {
-                        print("Capture button clicked!")
+                        captureButtonClicked()
                     }
                     .onHover { isHovered in
                         self.hoveringCaptureButton = isHovered
